b269824f84308b13072d3a729702d8842231807c
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-12-01 21:28:17 +0600

    adding

--- src/pages/Home/ChefRecommends/ChefRecommends.jsx
+++ src/pages/Home/ChefRecommends/ChefRecommends.jsx
@@ -6,14 +6,14 @@ import ProductCard from "../../Shared/ProductCard/ProductCard";
 const ChefRecommends = () => {
    const {recommendedMenu} = useContext(AuthContext);
    const limitedItems = recommendedMenu?.slice(0, 3) || [];
-   
+
   return (
     <section className="pt-16">
       <SectionTitle
         subHeading={"CHEF RECOMMENDS"}
         heading={"CHEF RECOMMENDS"}
       ></SectionTitle>
-
+  
       <div className="container mx-auto grid grid-cols-3 gap-16 p-10 justify-center items-center">
         {
             limitedItems.map(items =>
